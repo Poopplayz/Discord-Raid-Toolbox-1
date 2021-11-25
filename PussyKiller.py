2103aebdd3d311f91d73eeda201077fb8a5be58b
Lososiik <89783149+Lososiik@users.noreply.github.com>
2021-11-25 17:04:25 +0100

    Update PussyKiller.py

--- PussyKiller.py
+++ PussyKiller.py
@@ -1756,7 +1756,7 @@ def spammer():
 
     if choice == '20':
 
-        print('Do not do this without the permission of the person to whom the bruteforce attack is conducted.')
+        print('Do not do this without the permission of the person to whom the bruteforce attack is conducted. It isnt my tool it is only remake original is by Social 404')
 
         id_to_token = base64.b64encode((input("Id of user: ")).encode("ascii"))
         id_to_token = str(id_to_token)[2:-1]
